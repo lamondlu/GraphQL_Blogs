7a4b725b4e4dcc1995c0bdc63a5978b7d4fe5c58
Lamond Lu <lunan@shintechchina.com>
2019-04-11 15:42:13 +0800

    upload the missing file

---
+++ Part IV/code/chapter4/ClientApp/app.js
@@ -0,0 +1,19 @@
+import React from 'react';  
+import ReactDOM from 'react-dom';  
+import GraphiQL from 'graphiql';  
+import fetch from 'isomorphic-fetch';  
+import 'graphiql/graphiql.css';  
+import './app.css';
+
+function graphQLFetcher(graphQLParams) {  
+  return fetch(window.location.origin + '/api/graphql', {
+    method: 'post',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(graphQLParams)
+  }).then(response => response.json());
+}
+
+ReactDOM.render(  
+  <GraphiQL fetcher={graphQLFetcher} />,
+  document.getElementById('app')
+);
